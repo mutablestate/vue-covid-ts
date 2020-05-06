ce9486f844cc6cf92fd051486a7b8945328bc256
mutablestate <benwebdev@gmail.com>
2020-05-06 13:11:26 +0700

    vue next webpack ts

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+	<header>
+		<nav>
+			<ul>
+				<li>
+					<router-link to="/">Home</router-link>
+				</li>
+				<li>
+					<router-link to="/about">About</router-link>
+				</li>
+			</ul>
+		</nav>
+	</header>
+
+	<router-view />
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <img src="../logo.png">
+  <h1>Hello Vue 3!</h1>
+  <button @click="store.commit('increment')">Clicked {{ store.state.count }} times.</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { useStore } from 'vuex'
+
+export default defineComponent({
+  name: 'Home',
+  setup() {
+		const store = useStore()
+		return {store}
+  }
+})
+</script>
+
+<style scoped>
+img {
+  width: 200px;
+}
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
