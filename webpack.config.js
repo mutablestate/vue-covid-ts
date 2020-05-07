f5e694f6de347276a3975aa4daffcd0cffb9de8f
mutablestate <benwebdev@gmail.com>
2020-05-07 10:06:49 +0700

    Fix prod build
    * remove `webpack-bundle-analyzer`

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,76 @@
 const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = (env = {}) => ({
-  mode: env.prod ? 'production' : 'development',
-  devtool: env.prod ? 'source-map' : 'cheap-module-eval-source-map',
-  entry: path.resolve(__dirname, './src/main.ts'),
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/'
-  },
-  resolve: {
-    extensions: ['.ts', '.js', '.vue', '.json'],
-    alias: {
-      // this isn't technically needed, since the default `vue` entry for bundlers
-      // is a simple `export * from '@vue/runtime-dom`. However having this
-      // extra re-export somehow causes webpack to always invalidate the module
-      // on the first HMR update and causes the page to reload.
-      vue: '@vue/runtime-dom'
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        use: 'vue-loader'
-      },
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader',
-        options: {
-          appendTsSuffixTo: [/\.vue$/]
-        }
-      },
-      {
-        test: /\.png$/,
-        use: {
-          loader: 'url-loader',
-          options: { limit: 8192 }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: { importLoaders: 1 }
-          },
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.postcss$/,
-        use: ['style-loader', 'postcss-loader']
+module.exports = (env = {}) => {
+  return {
+    mode: env.prod ? 'production' : 'development',
+    devtool: env.prod ? 'source-map' : 'cheap-module-eval-source-map',
+    entry: path.resolve(__dirname, './src/main.ts'),
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, './dist'),
+      publicPath: '/dist/'
+    },
+    resolve: {
+      extensions: ['.ts', '.js', '.vue', '.json'],
+      alias: {
+        // this isn't technically needed, since the default `vue` entry for bundlers
+        // is a simple `export * from '@vue/runtime-dom`. However having this
+        // extra re-export somehow causes webpack to always invalidate the module
+        // on the first HMR update and causes the page to reload.
+        vue: '@vue/runtime-dom'
       }
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false
-    })
-  ],
-  devServer: {
-    inline: true,
-    hot: true,
-    stats: 'minimal',
-    contentBase: __dirname,
-    overlay: true,
-    historyApiFallback: {
-      index: 'index.html'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: 'vue-loader'
+        },
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          loader: 'ts-loader',
+          options: {
+            appendTsSuffixTo: [/\.vue$/]
+          }
+        },
+        {
+          test: /\.png$/,
+          use: {
+            loader: 'url-loader',
+            options: { limit: 8192 }
+          }
+        },
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: { importLoaders: 1 }
+            },
+            'postcss-loader'
+          ]
+        },
+        {
+          test: /\.postcss$/,
+          use: ['style-loader', 'postcss-loader']
+        }
+      ]
+    },
+    plugins: [
+      new VueLoaderPlugin()
+    ],
+    devServer: {
+      inline: true,
+      hot: true,
+      stats: 'minimal',
+      contentBase: __dirname,
+      overlay: true,
+      historyApiFallback: {
+        index: 'index.html'
+      }
     }
   }
-});
+};
